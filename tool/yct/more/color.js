5d655003414a641abaccad1cd84377ec36eb7ef3
Xe <3312902243@qq.com>
2025-10-21 13:07:56 +0800

    111

---
+++ tool/yct/more/color.js
@@ -0,0 +1,596 @@
+const color = [
+  // #FFFFFF
+  {
+    number: "C1",
+    type: "光泽",
+    color: "#FFFFFF",
+    rgb: [255, 255, 255],
+    atype: "",
+    contentText: "白",
+    description: "基本色",
+    sameColor: "1",
+  },
+  // #000000
+  {
+    number: "C2",
+    type: "光泽",
+    color: "#000000",
+    rgb: [0, 0, 0],
+    atype: "",
+    contentText: "黑",
+    description: "基本色",
+    sameColor: "2",
+  },
+  // #EE1D23
+  {
+    number: "C3",
+    type: "光泽",
+    color: "#EE1D23",
+    rgb: [238, 29, 35],
+    atype: "",
+    contentText: "红",
+    description: "基本色",
+    sameColor: "3",
+  },
+  // #FFDC01
+  {
+    number: "C4",
+    type: "光泽",
+    color: "#FFDC01",
+    rgb: [255, 220, 1],
+    atype: "",
+    contentText: "黄",
+    description: "基本色",
+    sameColor: "4",
+  },
+  // #015AAA
+  {
+    number: "C5",
+    type: "光泽",
+    color: "#015AAA",
+    rgb: [1, 90, 170],
+    atype: "",
+    contentText: "青",
+    description: "基本色",
+    sameColor: "5",
+  },
+  // #006733
+  {
+    number: "C6",
+    type: "光泽",
+    color: "#006733",
+    rgb: [0,103,51],
+    atype: "",
+    contentText: "绿",
+    description: "基本色",
+    sameColor: "6",
+  },
+  // #C94035
+  {
+    number: "C7",
+    type: "光泽",
+    color: "#C94035",
+    rgb: [201, 64, 53],
+    atype: "",
+    contentText: "棕",
+    description: "基本色",
+    sameColor: "7",
+  },
+  // #D1D2D4
+  {
+    number: "C8",
+    type: "金属",
+    color: "#D1D2D4",
+    rgb: [209, 210, 212],
+    atype: "",
+    contentText: "银",
+    description: "基本色",
+    sameColor: "8",
+  },
+  // #DBAF31
+  {
+    number: "C9",
+    type: "金属",
+    color: "#DBAF31",
+    rgb: [219, 175, 49],
+    atype: "",
+    contentText: "金",
+    description: "基本色",
+    sameColor: "9",
+  },
+  // #CC7239
+  {
+    number: "C10",
+    type: "金属",
+    color: "#CC7239",
+    rgb: [204, 114, 57],
+    atype: "",
+    contentText: "铜",
+    description: "基本色",
+    sameColor: "10",
+  },
+  // #B3BBBE
+  {
+    number: "C11",
+    type: "半光泽",
+    color: "#B3BBBE",
+    rgb: [179, 187, 190],
+    atype: "",
+    contentText: "灰",
+    description: "现用美海军机色",
+    sameColor: "51",
+  },
+  // #6D5E3B
+  {
+    number: "C12",
+    type: "半光泽",
+    color: "#6D5E3B",
+    rgb: [109,94,59],
+    atype: "",
+    contentText:"橄榄",
+    description:"美陆空军机色",
+    sameColor: "52",
+  },
+  // #667981
+  {
+    number: "C13",
+    type: "半光泽",
+    color: "#667981",
+    rgb: [102,121,129],
+    atype: "",
+    contentText:"中性灰",
+    description:"美陆空军机底色",
+    sameColor: "53",
+  },
+  // #204E5A
+  {
+    number: "C14",
+    type: "半光泽",
+    color: "#204E5A",
+    rgb: [32,78,90],
+    atype: "",
+    contentText:"海蓝色",
+    description:"美陆军机色",
+    sameColor: "54",
+  },
+  // #00381F
+  {
+    number: "C15",
+    type: "半光泽",
+    color: "#00381F",
+    rgb: [0, 56, 31],
+    atype: "",
+    contentText:"暗绿色",
+    description:"日海军机上面色",
+    sameColor: "59",
+  },
+  // #20543F
+  {
+    number: "C16",
+    type: "半光泽",
+    color: "#20543F",
+    rgb: [32, 84, 63],
+    atype: "",
+    contentText:"深绿色",
+    description:"日陆军机色",
+    sameColor: "60",
+  },
+  // #50534B
+  {
+    number: "C17",
+    type: "半光泽",
+    color: "#50534B",
+    rgb: [89, 90, 79],
+    atype: "",
+    contentText:"RLM71暗绿",
+    description:"德海军机上面色",
+  },
+  // #233C50
+  {
+    number: "C18",
+    type: "半光泽",
+    color: "#233C50",
+    rgb: [35, 60, 80],
+    atype: "",
+  },
+  // #9F8237
+  {
+    number: "C19",
+    type: "半光泽",
+    color: "#9F8237",
+    rgb: [159, 130, 55],
+    atype: "",
+  },
+  // #4FBAA1
+  {
+    number: "C20",
+    type: "半光泽",
+    color: "#4FBAA1",
+    rgb: [79, 186, 161],
+    atype: "",
+  },
+  // #B49241
+  {
+    number: "C21",
+    type: "半光泽",
+    color: "#B49241",
+    rgb: [180, 146, 65],
+    atype: "",
+  },
+  // #4F493C
+  {
+    number: "C22",
+    type: "半光泽",
+    color: "#4F493C",
+    rgb: [79, 73, 60],
+    atype: "",
+  },
+  // #415D47
+  {
+    number: "C23",
+    type: "半光泽",
+    color: "#415D47",
+    rgb: [65, 93, 71],
+    atype: "",
+  },
+  // #727A5E
+  {
+    number: "C25",
+    type: "半光泽",
+    color: "#727A5E",
+    rgb: [114, 122, 94],
+    atype: "",
+  },
+  // #D7E1AC
+  {
+    number: "C26",
+    type: "半光泽",
+    color: "#D7E1AC",
+    rgb: [215, 225, 172],
+    atype: "",
+  },
+  // #B6A811
+  {
+    number: "C27",
+    type: "半光泽",
+    color: "#B6A811",
+    rgb: [182, 168, 17],
+    atype: "",
+  },
+  // #231F20
+  {
+    number: "C28",
+    type: "金属",
+    color: "#231F20",
+    rgb: [35, 31, 32],
+    atype: "",
+  },
+  // #552E34
+  {
+    number: "C29",
+    type: "半光泽",
+    color: "#552E34",
+    rgb: [85, 46, 52],
+    atype: "",
+  },
+  // #FFFFFF
+  {
+    number: "C30",
+    type: "F",
+    color: "#FFFFFF",
+    rgb: [255, 255, 255],
+    atype: "",
+  },
+  // #909694
+  {
+    number: "C31",
+    type: "半光泽",
+    color: "#909694",
+    rgb: [144, 150, 148],
+    atype: "",
+  },
+  // #3A555A
+  {
+    number: "C32",
+    type: "半光泽",
+    color: "#3A555A",
+    rgb: [58, 85, 90],
+    atype: "",
+  },
+  // #000000
+  {
+    number: "C33",
+    type: "F",
+    color: "#000000",
+    rgb: [0, 0, 0],
+    atype: "",
+  },
+  // #008CD6
+  {
+    number: "C34",
+    type: "光泽",
+    color: "#008CD6",
+    rgb: [0, 140, 214],
+    atype: "",
+  },
+  // #A7BBC7
+  {
+    number: "C35",
+    type: "半光泽",
+    color: "#A7BBC7",
+    rgb: [167, 187, 199],
+    atype: "",
+  },
+  // #053C3E
+  {
+    number: "C36",
+    type: "半光泽",
+    color: "#053C3E",
+    rgb: [5, 60, 62],
+    atype: "",
+  },
+  // #485D56
+  {
+    number: "C37",
+    type: "半光泽",
+    color: "#485D56",
+    rgb: [72, 93, 86],
+    atype: "",
+  },
+  // #314836
+  {
+    number: "C38",
+    type: "F",
+    color: "#314836",
+    rgb: [49, 72, 54],
+    atype: "",
+  },
+  // #BF9D5A
+  {
+    number: "C39",
+    type: "F",
+    color: "#BF9D5A",
+    rgb: [191, 157, 90],
+    atype: "",
+  },
+  // #113C41
+  {
+    number: "C40",
+    type: "F",
+    color: "#113C41",
+    rgb: [17, 60, 65],
+    atype: "",
+  },
+  // #492F32
+  {
+    number: "C41",
+    type: "F",
+    color: "#492F32",
+    rgb: [73, 47, 50],
+    atype: "",
+  },
+  // #3C3032
+  {
+    number: "C42",
+    type: "半光泽",
+    color: "#3C3032",
+    rgb: [60, 48, 50],
+    atype: "",
+  },
+  // #6A4A31
+  {
+    number: "C43",
+    type: "半光泽",
+    color: "#6A4A31",
+    rgb: [106, 74, 49],
+    atype: "",
+  },
+  // #C9A965
+  {
+    number: "C44",
+    type: "半光泽",
+    color: "#C9A965",
+    rgb: [201, 169, 101],
+    atype: "",
+  },
+  // #C0B77F
+  {
+    number: "C45",
+    type: "半光泽",
+    color: "#C0B77F",
+    rgb: [192, 183, 127],
+    atype: "",
+  },
+  // #FFFFFF
+  {
+    number: "C46",
+    type: "光泽",
+    color: "#FFFFFF",
+    rgb: [255, 255, 255],
+    atype: "",
+  },
+  // #E60012
+  {
+    number: "C47",
+    type: "光泽",
+    color: "#E60012",
+    rgb: [230, 0, 18],
+    atype: "",
+  },
+  // #FFD900
+  {
+    number: "C48",
+    type: "光泽",
+    color: "#FFD900",
+    rgb: [255, 217, 0],
+    atype: "",
+  },
+  // #EB6100
+  {
+    number: "C49",
+    type: "光泽",
+    color: "#EB6100",
+    rgb: [235, 97, 0],
+    atype: "",
+  },
+
+  // ====水性====
+
+  // #FFFFFF
+  {
+    number: "H1",
+    type: "光泽",
+    color: "#FFFFFF",
+    rgb: [255, 255, 255],
+    atype: "",
+  },
+  // #000000
+  {
+    number: "H2",
+    type: "光泽",
+    color: "#000000",
+    rgb: [0, 0, 0],
+    atype: "",
+  },
+  // #EE1D23
+  {
+    number: "H3",
+    type: "光泽",
+    color: "#EE1D23",
+    rgb: [238, 29, 35],
+    atype: "",
+  },
+  // #FFDC01
+  {
+    number: "H4",
+    type: "光泽",
+    color: "#FFDC01",
+    rgb: [255, 220, 1],
+    atype: "",
+  },
+  // #015AAA
+  {
+    number: "H5",
+    type: "光泽",
+    color: "#015AAA",
+    rgb: [1, 90, 170],
+    atype: "",
+  },
+  // #00904C
+  {
+    number: "H6",
+    type: "光泽",
+    color: "#00904C",
+    rgb: [0, 144, 76],
+    atype: "",
+  },
+  // #C94035
+  {
+    number: "H7",
+    type: "光泽",
+    color: "#C94035",
+    rgb: [201, 64, 53],
+    atype: "",
+  },
+  // #D1D2D4
+  {
+    number: "H8",
+    type: "金属",
+    color: "#D1D2D4",
+    rgb: [209, 210, 212],
+    atype: "",
+  },
+  // #DBAF31
+  {
+    number: "H9",
+    type: "金属",
+    color: "#DBAF31",
+    rgb: [219, 175, 49],
+    atype: "",
+  },
+  // #CC7239
+  {
+    number: "H10",
+    type: "金属",
+    color: "#CC7239",
+    rgb: [204, 114, 57],
+    atype: "",
+  },
+  // #FFFFFF
+  {
+    number: "H11",
+    type: "消光",
+    color: "#FFFFFF",
+    rgb: [255, 255, 255],
+    atype: "",
+  },
+  // #231F20
+  {
+    number: "H12",
+    type: "消光",
+    color: "#685932",
+    rgb: [35, 31, 32],
+    atype: "",
+  },
+  // #DA2128
+  {
+    number: "H13",
+    type: "消光",
+    color: "#DA2128",
+    rgb: [218, 33, 40],
+    atype: "",
+  },
+  // #EF3E22
+  {
+    number: "H14",
+    type: "光泽",
+    color: "#EF3E22",
+    rgb: [239, 62, 34],
+    atype: "",
+  },
+  // #0D4DA1
+  {
+    number: "H15",
+    type: "光泽",
+    color: "#0D4DA1",
+    rgb: [13, 77, 161],
+    atype: "",
+  },
+  // #29A449
+  {
+    number: "H16",
+    type: "光泽",
+    color: "#29A449",
+    rgb: [41, 164, 73],
+    atype: "",
+  },
+  // #603336
+  {
+    number: "H17",
+    type: "半光",
+    color: "#603336",
+    rgb: [96, 51, 54],
+    atype: "",
+  },
+  // #231F20
+  {
+    number: "H18",
+    type: "金属",
+    color: "#231F20",
+    rgb: [35, 31, 32],
+    atype: "",
+  },
+  // #F17097
+  {
+    number: "H19",
+    type: "光泽",
+    color: "#F17097",
+    rgb: [241, 112, 151],
+    atype: "",
+  },
+];
+
+const hbColor = [];
+
+window.hbColorList = hbColor;
+window.colorList = color;
